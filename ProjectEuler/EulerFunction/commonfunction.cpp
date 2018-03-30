673dd2ac9c6ce85d60c267c31f090609349ef969
smokindinesh <smokindinesh@gmail.com>
2018-03-30 11:59:27 +0545

    Problem 1 and Problem 2 added

---
+++ ProjectEuler/EulerFunction/commonfunction.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include "eulerfunction.h"
+
+
+Eulerfunction::Eulerfunction()
+{
+}
+
+
+Eulerfunction::~Eulerfunction()
+{
+}
+
+void Eulerfunction::helloTest()
+{
+	std::cout << "Hello World Test" << std::endl;
+}
---
+++ ProjectEuler/EulerFunction/commonfunction.h
@@ -0,0 +1,9 @@
+#pragma once
+class CommonFunction
+{
+public:
+	CommonFunction();
+	~CommonFunction();
+	void helloTest();
+};
+
